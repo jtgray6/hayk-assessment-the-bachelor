2960414e5fe7c74d300d0ab9672c097a6a551faf
Jeremy Gray <jeremytgray6@gmail.com>
2020-01-10 10:55:54 -0700

    Solved rounding error using to_f and returned rspec to original value

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -48,7 +48,7 @@ def get_average_age_for_season(data, season)
       contestants.each do |contestant|
         season_ages << contestant["age"].to_i
       end
-      average = season_ages.sum/season_ages.length
+      average = (season_ages.sum.to_f/season_ages.count.to_f).round
       # binding.pry
       return average
     end
